0b343ef91dcc8215ebc78dfbcf4de4e63dc91e55
Travis <travisstew214@gmail.com>
2019-10-04 20:37:05 -0400

    update time formula

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,17 +1,8 @@
   var fire = firebase.database();
-  var d = 1569210248676;
-  var date = new Date();
-  var timeNow = date.getTime();
 
+ 
 
-
-  function toString(m){
-    return moment(m).toString();
-  }
-  function toISO(m){
-    return moment(m).toISOString();
-  }
-
+  
 
   $('#submit').on('click', function(e){
     e.preventDefault();
@@ -33,15 +24,73 @@
 
 fire.ref().on('child_added', function(e){
   
-  $('tbody').append(
-    `<tr>
-      <td>${e.val().Name}</td>
-      <td>${e.val().Destination}</td>
-      <td>${e.val().First_Train}</td>
-      <td>${e.val().Frequency}</td>
-      
-    </tr>`
+  // $('tbody').append(
+  //   `<tr>
+  //     <td>${e.val().Name}</td>
+  //     <td>${e.val().Destination}</td>
+  //     <td>${e.val().Frequency}</td>
 
-  );
+  //     <td>${e.val().First_Train}</td>
+
+  //     <td>${e.val().Frequency}</td>
+  //  </tr>`
+
+  // );
+  var startTime = e.val().First_Train;
+   var freq = parseInt(e.val().Frequency);
+   var timeNow = moment();
+  console.log(freq);
+  console.log(startTime);
   
+  
+   var diffInTime = moment(startTime,"hh:mm ").diff(timeNow,'m');
+   var freqTimeDiff;
+   var trainNewTime;
+   var minsBeforeTrain;
+   console.log(diffInTime);
+
+   //if the diffInTime is positive , (it means the start time is ahead of the current time)
+   if(diffInTime > 0){
+     freqTimeDiff = diffInTime / freq;
+    var c = Math.floor(freqTimeDiff);
+    var d = c * freq;
+    console.log(moment(startTime,"hh:mm").subtract(d,'m').format('LT'));
+     trainNewTime = moment(startTime,"hh:mm").subtract(d,'m');
+   }else{
+     freqTimeDiff = diffInTime / freq;
+     var c = Math.floor(freqTimeDiff);
+     var d = (c * freq) * -1;
+     console.log(c);
+     console.log(moment(startTime,"hh:mm").add(d,'m').format('LT'));
+     trainNewTime =  moment(startTime,"hh:mm").add(d,'m');    
+   }
+ 
+   minsBeforeTrain =  trainNewTime.diff(moment(),'m');
+    
+  var tr = $('<tr>');
+  var td1 = $('<td>');
+  var td2 = $('<td>');
+  var td3 = $('<td>');
+  var td4 = $('<td>');
+  var td5 = $('<td>');
+
+   td1.html(e.val().Name);
+   td2.html(e.val().Destination);
+   td3.html(e.val().Frequency);  
+   td4.html(trainNewTime.format("LT"));
+   td5.html(minsBeforeTrain);
+
+      tr.append(td1,td2,td3,td4,td5)
+   $('tbody').append(tr)
+
+
+   console.log( e.val().First_Train );
+
+   console.log(moment(e.val().First_Train,"hh:mm"));
+   console.log( parseInt(e.val().Frequency));
+
+
+  
+   
+
   });
